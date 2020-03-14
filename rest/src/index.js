65222577532a0eefa787876e4aa47e14331244a4
Janbanwan <janne.antila@hotmail.com>
2020-03-14 23:10:17 +0000

    updated port

--- rest/src/index.js
+++ rest/src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 const version = "v1";
 
 app.use(express.json());
 app.use(`/api/${version}`, routes);
 
-const port = 3000;
+const port = 8000;
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
